8d0c6006be4049e4553a628275c84caafc496ccb
korwin <levozavr@mail.ru>
2019-06-30 15:24:10 +0300

    example with mongo

--- backend/api/users.py
+++ backend/api/users.py
@@ -1,9 +1,12 @@
 from flask_restful import Resource
+import json
+from bson import json_util
 
 
 class Users(Resource):
     def get(self):
         from backend import mongo
         mongo.db.test.insert_one({'lol': 'lol'})
-        test = mongo.db.test.find_one().drop('_id', None)
-        return test
+        return json.loads(json.dumps([i for i in mongo.db.test.find()],
+                                     sort_keys=True, indent=4, default=json_util.default))
+
