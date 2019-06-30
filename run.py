f1e0892fd2e71fea520815d025aa37e8aeca34ba
korwin <levozavr@mail.ru>
2019-06-30 12:35:14 +0300

    add port

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import os
 
 from backend.app import create_app
+os.environ.setdefault('FLASK_ENV', 'development')
 
 
 if __name__ == '__main__':
   env_name = os.getenv('FLASK_ENV')
+  print(env_name)
   app = create_app(env_name)
-  app.run()
+  app.run(port=8080)
