104fccd3f8dbce204e06dad0b3be90cbe8bb6c9b
korwin <levozavr@mail.ru>
2019-07-12 13:54:45 +0300

    add pylint and fix small errors

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import os
 
 from backend import create_app
+
 os.environ.setdefault('FLASK_ENV', 'development')
 
 
 if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
-  port = int(os.getenv('PORT', 8080))
-  app = create_app(env_name)
-  app.run(host='0.0.0.0', port=port)
+    env_name = os.getenv('FLASK_ENV')
+    port = int(os.getenv('PORT', 8080))
+    app = create_app(env_name)
+    app.run(host='0.0.0.0', port=port)
